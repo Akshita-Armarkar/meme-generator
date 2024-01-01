e10f171a2701a448009d46c969bad23144ab16e3
Akshita <akshitaarmarkar96@gmail.com>
2024-01-02 03:01:15 +0530

    memes data added

--- meme-generator/src/components/Meme/Meme.js
+++ meme-generator/src/components/Meme/Meme.js
@@ -1,18 +1,27 @@
 import './Meme.css'
+import {test} from './memes.js'
 export default function Meme() {
+
+    function generateRandomNumber() {
+        let random = Math.floor(Math.random() * (100));
+        if(test.success) {
+            console.log(test.data.memes[random].id)
+        }
+    }
+
     return (
         <form className='form'>
             <div className='inputs'>
                 <div className='input-group'>
-            <label for="top">Top Text:</label>
+            <label htmlFor="top">Top Text:</label>
             <input type="text" id="fname" name="top" placeholder="Shut up"/>
             </div>
             <div className='input-group'>
-            <label for="bottom">Bottom Text:</label>
+            <label htmlFor="bottom">Bottom Text:</label>
             <input type="text" id="fname" name="bottom" placeholder="And take my money"/>
             </div>
             </div>
-            <button className='button' type='submit'>Get a new meme image  🖼</button>
+            <button onClick={generateRandomNumber} className='button' type='button'>Get a new meme image  🖼</button>
         </form>
     )
 }
