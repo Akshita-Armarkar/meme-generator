83fc193bd9c0fcf6043416dada155456f4c83cf6
Akshita <akshitaarmarkar96@gmail.com>
2024-01-04 17:50:29 +0530

    output updated

--- meme-generator/src/components/Meme/Meme.js
+++ meme-generator/src/components/Meme/Meme.js
@@ -3,11 +3,28 @@ import {test} from './memes.js';
 import {useState} from 'react'
 export default function Meme() {
 
-    const [random, setRandom] = useState(0)
+    const [meme, setMeme] = useState({
+        topText : 'top text', 
+        bottomText : 'bottom text', 
+        randomImage : 'https://i.imgflip.com/30b1gx.jpg'
+    })
+    const [allMemeImages, setAllMemeImages] = useState(test);
 
-    function generateRandomNumber() {
-        setRandom(Math.floor(Math.random() * (100)))
-        console.log(random);
+    function memeGenerator() { 
+        setMeme(prev => {
+            return {...prev, randomImage : allMemeImages.data.memes[Math.floor(Math.random() * (100))].url}
+        }
+        )
+    }
+
+    function setInputs(e) {
+        e.target.name === 'top' ?  setMeme( prev => {
+            return {...prev, topText : e.target.value}
+        }) : setMeme( prev => {
+            return {...prev, bottomText : e.target.value}
+        }) 
+
+        console.log(meme)
     }
 
     return (
@@ -15,15 +32,19 @@ export default function Meme() {
             <div className='inputs'>
                 <div className='input-group'>
             <label htmlFor="top">Top Text:</label>
-            <input type="text" id="fname" name="top" placeholder="Shut up"/>
+            <input type="text" id="fnametop" name="top" placeholder="Shut up" onChange={setInputs}/>
             </div>
             <div className='input-group'>
             <label htmlFor="bottom">Bottom Text:</label>
-            <input type="text" id="fname" name="bottom" placeholder="And take my money"/>
+            <input type="text" id="fnamebottom" name="bottom" placeholder="And take my money" onChange={setInputs}/>
             </div>
             </div>
-            <img src={test.data.memes[random].url} alt='memeimage' className='output-image'/>
-            <button onClick={generateRandomNumber} className='button' type='button'>Get a new meme image  🖼</button>
+            <div className='output'>
+                <span>{meme.topText}</span>
+            <img src={meme.randomImage} alt='memeimage' className='output-image'/>
+                <span>{meme.bottomText}</span>
+            </div>
+            <button onClick={memeGenerator} className='button' type='button'>Get a new meme image  🖼</button>
         </form>
     )
 }
